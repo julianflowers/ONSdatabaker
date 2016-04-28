91854b8d5a874744db9c0a5a261b5f7933df095d
MikeData <Mikelovesbooks@gmail.com>
2016-04-28 09:23:05 +0100

    Changed to a clean output structure. Added more in depth commenting to structure_csv_default.py

--- ONSdatabaker/structure_csv_default.py
+++ ONSdatabaker/structure_csv_default.py
@@ -1,59 +1,77 @@
 """
 Template/Options file for altering the structure of the .csv flatfile output.
 
+Terminology:
+
+standard dimensions - the dimensions/columns that must be present in each and every file output by
+databaker. You must always have an observations/values column as a standard dimension. It must ALWAYS 
+be the first one.
+
+topic dimensions - the dynamic dimensions (varying in number, minimum of one). These get added to the 
+right hand side of your standard columns. 
+
 """
+
 # = Standard Dimensions ========
 
-# Start = list of standard headers.
-start = "observation,data_marking,statistical_unit_eng,statistical_unit_cym,measure_type_eng,measure_type_cym,observation_type,empty,obs_type_value,unit_multiplier,unit_of_measure_eng,unit_of_measure_cym,confidentuality,empty1,geographic_area,empty2,empty3,time_dim_item_id,time_dim_item_label_eng,time_dim_item_label_cym,time_type,empty4,statistical_population_id,statistical_population_label_eng,statistical_population_label_cym,cdid,cdiddescrip,empty5,empty6,empty7,empty8,empty9,empty10,empty11,empty12"
+# Start = list of headers for standard dimensions
+start = "Observation,Data Marking,Geographic Area,Time"
 
 # Dimension names (as strings!)
-dimension_names = ['OBS', 'DATAMARKER', 'STATUNIT', 'MEASURETYPE', 'UNITMULTIPLIER', 'UNITOFMEASURE', 'GEOG', 'TIME', 'TIMEUNIT', 'STATPOP']
+# This defines how you'll assign data to the standard columns within the recipes
+# MUST have OBS. The others are optional.
+dimension_names = ['OBS', 'DATAMARKER', 'GEOG', 'TIME']
 
+
+# --- DONT CHANGE this code
 # Create variables
 for i, item in enumerate(reversed(dimension_names)):
     exec(item+"=-i")
 
 __all__ = list(dimension_names) # don't expose unnecessary items when using `from foo import *`
+# --- end of DONT CHANGE
 
-# Columns to skip after each dimension is output. order as above
-SKIP_AFTER = {OBS: 0,            # 1..2  MANDATORY, must be here, must be called OBS
-              DATAMARKER: 0,     # 2..3
-              STATUNIT: 1,       # 3..5
-              MEASURETYPE: 4,    # 5..10
-              UNITMULTIPLIER: 0, # 10..11
-              UNITOFMEASURE: 3,  # 11..15
-              GEOG: 2,           # 15..18/19
-              TIME: 1,           # 18/19..21
-              TIMEUNIT: 1,       # 21..23/24
-              STATPOP:11}        # 23/24..36/37
+
+# Columns to skip after each dimension/column is output. Order should match dimension_names.
+# The number specifies columns to skip (so you can more easily matcha  required input structure)
+SKIP_AFTER = {OBS: 0,            # 1  MANDATORY, must be here, must be called OBS
+              DATAMARKER: 0,      
+              GEOG: 0,            
+              TIME: 0,           
+              }        
 
 # = Topic Dimensions ========
 
-# Repeat - list of headers to be repeated for each topic dimension
-repeat = "dim_id_{num},dimension_label_eng_{num},dimension_label_cym_{num},dim_item_id_{num},dimension_item_label_eng_{num},dimension_item_label_cym_{num},is_total_{num},is_sub_total_{num}"
+# Repeat - a comma delimited string of headers to be repeated for each topic dimension, use {num} to count
+# i.e "Value {num},Blank {num},Name {num}" would extract topic dimensions as:
+# Value 1, Blank 1, Name 1, Value 2, Blank 2, Name 2, etc
+repeat = "dim_id_{num}"
 
 
-# Where in the repeat do you want to output the dimensions name and value?
+# Where in the repeat do you want to output the dimension name and value?
+# Example ([value,'', name]) would output three columns per topic dimensions, as above example
 def get_topic_headers(name, value):  # DONT alter this
-    return ([name, name, '', value, value, '', '', ''])   # Change this line
+    return ([value])   # Change this line
 
-# Where are the values? (should match the above (minus the 'name entries)
-value_spread = ['', '', '', 'value', 'value', '', '', '']
+# Where are the values? (should mirror the above (minus the 'name entries)
+# So, ['value', '', ''] for our value-blank-name example
+value_spread = ['value']
 
-# do you want to output the 'name' value in the header of the value columns?
-topic_headers_as_dims = False
+# do you want to output the 'name' value in the header of the 'value' columns?
+topic_headers_as_dims = True
 
 
 
-# ====================== S-P-E-C-I-A-L handling ========================== (..fallout much?)
-# Use the following to decide which STANDARD dimensionss should get special handling
+# ====================== S-P-E-C-I-A-L handling ==========================
+# the following are laragely ONS specific but can be swtiched on and off for anyones use
 
-# Standard Dimensions that need to be outputted twice in a row (i.e item|label combos)
-SH_Repeat = [TIME, STATPOP]
+# Standard Dimensions/columns that need to be outputted twice in a row (i.e item|label combos)
+# if you use this you MUST account for the extra column with an extra heading in 'start'
+SH_Repeat = []
 
-# Do we want to create a TIMEUNIT dimension using a TIME dimension - ONS specific
-SH_Create_ONS_time = True
+# Do we want to create a TIMEUNIT dimension using a TIME dimension - very ONS specific
+SH_Create_ONS_time = False
 
-# Do you want to split the OBS, placing non float data into your next column.
+# Do you want to split the OBS, placing non float data into something specified in dimension_names
+# If not, change it to '= False'
 SH_Split_OBS = DATAMARKER
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ Transform Excel spreadsheets
 
 conf = dict(
     name='ONSdatabaker',
-    version='1.1.2',
+    version='1.1.3',
     description="Excel Output Transformation",
     long_description=long_desc,
     classifiers=[
